b7dd01b4609d19c1ac60678e0f6fbebb93f2076c
Victor Delgado <valexdelgado@gmail.com>
2022-10-13 14:26:28 -0700

    finished up

--- python-oo-practice/wordfinder.py
+++ python-oo-practice/wordfinder.py
@@ -1,7 +1,13 @@
 """Word Finder: finds random words from a dictionary."""
-from random import randint
+from random import randint, choice
 
 class WordFinder:
+    '''
+        # I'm not a python expert but I think the accepted best practice is to use a context manager for opening files. 
+        The solution code uses open() but doesn't close the file, isn't that dangerous? 
+    '''
+
+
     def __init__(self, file):
         self.file = file
         with open(self.file) as f:
@@ -11,8 +17,12 @@ class WordFinder:
     def read_file(self):
         return ([word.strip() for word in self.contents])
 
-    def get_random(self, num):
-        pass
+    def get_random(self):
+        return choice(self.contents)
 
 words = WordFinder('python-oo-practice\words.txt')
-print(words.read_file())
+print(words.get_random())
+print(words.get_random())
+print(words.get_random())
+print(words.get_random())
+print(words.get_random())
